73a15f2e8ec3d15215f38bfce7c66cab31c28a4d
Alexander Gorelik <Alexander.Gorelik@4finance.com>
2024-02-26 10:21:39 +0100

    methods 3

--- config.php
+++ config.php
@@ -1,6 +1,15 @@
 <?php
   header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
   header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
   header("Content-Type: application/json; charset=UTF-8");
+
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+      header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    }
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+      header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+    }
+  }
 ?>
