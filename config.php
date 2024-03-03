2c3c419955cd145dd5e43aeb2c5afd98ffdd8a57
Alexander Gorelik <Alexander.Gorelik@4finance.com>
2024-03-03 15:26:55 +0100

    uplaod image 2

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Headers: Overwrite, Destination, Depth, User-Agent, X-File-Size, If-Modified-Since, X-File-Name, Cache-Control, Origin, Content-Type, Content-Length, Authorization, X-Custom-Header, X-Requested-With, X-Auth-Token, Accept, Key, api-csrf');
-  header("Content-Type: application/json; charset=UTF-8");
+  // header('Access-Control-Allow-Origin: *');
+  // header('Access-Control-Allow-Headers: Overwrite, Destination, Depth, User-Agent, X-File-Size, If-Modified-Since, X-File-Name, Cache-Control, Origin, Content-Type, Content-Length, Authorization, X-Custom-Header, X-Requested-With, X-Auth-Token, Accept, Key, api-csrf');
+  // header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Headers: *");
+  header("Access-Control-Allow-Methods: *");
 
   if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
